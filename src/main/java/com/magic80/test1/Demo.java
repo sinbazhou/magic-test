b8fbdf41c3ee7cb811a72fe55a0b988c70ba6588
周永振 <zhouyongzhen@daojia-inc.com>
2018-12-28 10:32:29 +0800

    d

---
+++ src/main/java/com/magic80/test1/Demo.java
@@ -0,0 +1,7 @@
+package com.magic80.test1;
+
+public class Demo {
+    public void test1(){
+        System.out.println("abc");
+    }
+}
---
+++ src/main/java/com/magic80/test1/TestController.java
@@ -0,0 +1,19 @@
+package com.magic80.test1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "b");
+        return map;
+    }
+}
